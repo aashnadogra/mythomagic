88ed417e174ddd30185902b91ee1e2c1e680fd08
Aashna Dogra <139170807+aashnadogra@users.noreply.github.com>
2024-12-26 16:59:58 +0530

    initialised frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import AuthForm from './AuthForm'; 
+
+const App = () => {
+  return (
+    <div>
+      <AuthForm />
+    </div>
+  );
+};
+
+export default App;
+
